019911c4ae62c02af75c36834bc244cfc262d7d1
elizabete silva <elizabete.airam@gmail.com>
2022-03-13 23:52:15 -0300

    title and redirects

--- src/Pages/Inicio.js
+++ src/Pages/Inicio.js
@@ -23,13 +23,14 @@ export default function Inicio() {
 
   return (
     <div className="App">
+       <h1 className="ui teal header">_mood moon</h1>
       <header className="App-header">
         <img src={lua} className="App-logo" alt="logo" />
         {moons.map((moon) => (
           <>
             <h2>hoje é dia de:</h2>
             <h2>{moon.simbolo_lunar}</h2>
-            <h1>{moon.fase_lunar}</h1>
+            <h1 class="titulo">{moon.fase_lunar}</h1>
 
             <h4>
               começou no dia {moon.dia} de {moon.mes} de {moon.ano}
